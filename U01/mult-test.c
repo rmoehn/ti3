0299ce92cd6b4b1cb5a124cb246ccb1f3244d3d8
Richard Möhn <richard.moehn@fu-berlin.de>
2012-10-18 20:30:03 +0200

    Add makefile and a apparently very wrong program

---
+++ U01/mult-test.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+#define MAXLINE 50
+
+/*
+ * Usage:
+ *     $ mult-test <filename>
+ *
+ * Read a text file line by line and checks whether the first line is the sum
+ * of the products in the following lines. Print 'correct' if that is the
+ * case, otherwise print 'incorrect'.
+ *
+ * Format of the input file:
+ *
+ *     -------------
+ *     <int>
+ *     <int> * <int>
+ *     <int> * <int>
+ *     ...
+ *     -------------
+ *
+ *  - The first line contains a single integer.
+ *  - All other lines contain two integers separated by " * ".
+ *  - Anything not complying with these rules will cause the program to exit
+ *    or to misbehave.
+ */
+
+int main(int argc, char *argv[])
+{
+    char *prog = argv[0];
+
+    // Make sure there is exactly one command-line argument (the file name)
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", prog);
+        return 1;
+    }
+
+    // Try and open the file
+    char *filename = argv[1];
+    FILE *fp;
+    if ((fp = fopen(filename, "r")) == NULL) {
+        fprintf(stderr, "%s: Can't open %s\n", prog, filename);
+        return 2;
+    }
+
+    // Try and read the first line
+    char *sum_string;
+    if (getline(sum_string, fp) == NULL) {
+        fprintf(stderr, "%s: Given file %s is empty.\n", prog, filename);
+        return 3;
+    }
+
+    // Read the contents to be the sum
+    int expected_sum;
+    if (sscanf(sum_line, "%d", &expected_sum) != 1) {
+        fprintf(stderr, "%s: Illegal format in %s.\n", prog, filename);
+        return 3;
+    }
+
+    // Read the remaining lines and sum them up
+    int sum = 0;
+    char *prod_line;
+    while (getline(prod_line, fp) != NULL) {
+        int fac1, fac2;
+        if (sscanf(prod_line, "%d * %d", &fac1, &fac2) != 2) {
+            fprintf(stderr, "%s: Illegal format in %s.\n", prog, filename);
+            return 3;
+        }
+
+        sum += fac1 * fac2;
+    }
+
+    printf(sum == expected_sum ? "correct\n" : "incorrect\n");
+
+    return 0;
+}
+
+// A shorthand.
+getline(char *out_string, FILE *fp) {
+    return fgets(out_string, MAXLINE, fp);
+}
