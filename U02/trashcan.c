0fc6e8d594ea20e9d5c6139a14ddb63ce3a1523e
Richard Möhn <richard.moehn@fu-berlin.de>
2012-11-07 20:12:20 +0100

    Trashcan fertig

--- U02/trashcan.c
+++ U02/trashcan.c
@@ -11,10 +11,19 @@
 
 #define BUFFSIZE 4096
 #define TRASHDIRMODE 0700
-#define TRASHDIRNAME .ti3_trash
+#define TRASHDIRNAME ".ti3_trash"
 
-int filecopy(char *, char *);
-int opt_mkhomedir(const char *);
+/* You'd better not read (nor use) this program. */
+
+int filecopy(const char *, const char *);
+char *filecat(char *, const char *, const char *);
+int filecatlen(const char *, const char *);
+char *get_trashname(char *, const char *);
+int get_trashnamelen(const char *);
+void opt_mktrashdir();
+void move_to_trash(const char *);
+void recover_from_trash(const char *);
+void finally_delete(const char *);
 
 int main(int argc, char *argv[])
 {
@@ -44,6 +53,58 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+/* Moves the file with the given name to the trash. */
+void move_to_trash(const char *filename)
+{
+    /* Create the trash if it doesn't exist already. */
+    opt_mktrashdir();
+
+    /* Obtain the absolute path to the file in the trash */
+    char trashname[get_trashnamelen(filename)];
+    get_trashname(trashname, filename);
+
+    /* Copy the file over */
+    if (filecopy(filename, trashname) != 0) {
+        err(2, "Error copying %s to %s.", filename, trashname);
+    }
+
+    /* Remove the original file */
+    if (unlink(filename) == -1) {
+        err(3, "Cannot remove source file %s.", filename);
+    }
+}
+
+/* Moves the given file from the trash to the current directory. */
+void recover_from_trash(const char *filename)
+{
+    /* Obtain the name of the file in the trash */
+    char trashname[get_trashnamelen(filename)];
+    get_trashname(trashname, filename);
+
+    /* Copy the file to the current directory */
+    if (filecopy(trashname, filename) != 0) {
+        err(2, "Error copying %s to current directory.", filename);
+    }
+
+    /* Remove the original file */
+    if (unlink(trashname) == -1) {
+        err(3, "Cannot remove file %s from trash.", trashname);
+    }
+}
+
+/* Ultimately removes the given file from the trash. */
+void finally_delete(const char *filename)
+{
+    /* Obtain the name of the file in the trash */
+    char trashname[get_trashnamelen(filename)];
+    get_trashname(trashname, filename);
+
+    /* Remove it */
+    if (unlink(trashname) == -1) {
+        err(3, "Cannot remove file %s from trash.", trashname);
+    }
+}
+
 /*
  * Copy the contents of a file into another not yet existing file.
  *
@@ -52,7 +113,7 @@ int main(int argc, char *argv[])
  * Return 2 on problems with the output file.
  * Return 3 on problems in the copying process.
  */
-int filecopy(char *infile, char *outfile)
+int filecopy(const char *infile, const char *outfile)
 {
     /* Open the input file */
     int in_fd = open(infile, O_RDONLY, 0);
@@ -94,35 +155,76 @@ int filecopy(char *infile, char *outfile)
     return 0;
 }
 
-/*
- * Creates a directory with the given name relative to the current
- * environment's home directory if it doesn't exist already.
- *
- * Return 0 on success.
- * Return 1 on problems with finding the home directory.
- * Return 2 on problems with creating the directory.
- */
-int opt_mkhomedir(const char *filename)
+/* Creates the trash directory if it does not exist yet.  */
+void opt_mktrashdir()
 {
+    /* Obtain the complete path of the trash directory */
+    char trashdirname[get_trashnamelen("")]; /* "" is trash only */
+    get_trashname(trashdirname, "");
+    if (trashdirname == NULL) {
+        err(
+            4,
+            "Cannot obtain a path to the trash directory %s.",
+            trashdirname
+        );
+    }
+
+    /* Create the directory if it doesn't exist already */
+    if (mkdir(trashdirname, TRASHDIRMODE) != 0) {
+        if (errno != EEXIST) {
+            err(5, "Cannot create trash directory %s.", trashdirname);
+        }
+    }
+}
+
+/*
+ * Returns the string length (including \0) of the absolute path of the given
+ * file in the trash.
+ */
+int get_trashnamelen(const char *filename) {
     /* Obtain the current home directory */
     const char *homedir = getenv("HOME");
     if (homedir == NULL) {
         return 1;
     }
 
-    /* Form the final homedir name and the given filename */
-    size_t dirname_len = strlen(homedir) + strlen(filename) + 2;
-    char dirname[dirname_len];
-    strcpy(dirname, homedir);
-    strcat(dirname, "/");
-    strcat(dirname, filename);
+    return strlen(homedir) + strlen(TRASHDIRNAME) + strlen(filename) + 3;
+}
 
-    /* Create the directory if it doesn't exist already */
-    if (mkdir(dirname, TRASHDIRMODE) != 0) {
-        if (errno != EEXIST) {
-            return 2;
-        }
+/*
+ * From a given filename, constructs its absolute path in the trash can.
+ */
+char *get_trashname(char *outfilename, const char *filename)
+{
+    /* Obtain the current home directory */
+    const char *homedir = getenv("HOME");
+    if (homedir == NULL) {
+        return NULL;
     }
 
-    return 0;
+    /* Creates the absolute path to the trash directory */
+    char trashdir[filecatlen(homedir, TRASHDIRNAME)];
+    filecat(trashdir, homedir, TRASHDIRNAME);
+
+    return filecat(outfilename, trashdir, filename);
+}
+
+/*
+ * Returns the string length of a path to be constructed by filecat.
+ */
+int filecatlen(const char *filename1, const char *filename2)
+{
+    return strlen(filename1) + strlen(filename2) + 2;
+}
+
+/*
+ * Joins two filenames with a /.
+ */
+char *filecat(char *outfilename, const char *filename1, const char *filename2)
+{
+    strcpy(outfilename, filename1);
+    strcat(outfilename, "/");
+    strcat(outfilename, filename2);
+
+    return outfilename;
 }
